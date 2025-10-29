ec816174afd8e3bf5cdd8e9e845a2dfe3503ad22
MrFattoush <fattash76@gmail.com>
2025-10-28 19:04:38 -0500

    Manual Page set up

--- ReceiptManager/screens/ManuallyAddScreen.tsx
+++ ReceiptManager/screens/ManuallyAddScreen.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+
 
 export default function ManuallyAddScreen() {
+
+  const [merchant, setMerchant] = useState('');
+  const [totalAmount, setTotalAmount] = useState('');
+  const [purchaseDate, setPurchaseDate] = useState('');
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Manual Entry</Text>
-      <Text style={styles.subtitle}>Form fields will be added here</Text>
+      <TextInput
+      style={styles.input}
+      placeholder="Merchant Name"
+      value={merchant}
+      onChangeText={setMerchant}
+      />
+
+      <TextInput
+      style={styles.input}
+      placeholder="Total Amount"
+      keyboardType="decimal-pad"
+      value={totalAmount}
+      onChangeText={setTotalAmount}
+      />
+
+      <TextInput
+      style={styles.input}
+      placeholder="Purchase Date"
+      keyboardType="numeric"
+      value={purchaseDate}
+      onChangeText={setPurchaseDate}
+      />
+
+
+      <Button title="Save Receipt" onPress={() => console.log('Submit pressed')}/>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -29,4 +62,8 @@ const styles = StyleSheet.create({
     color: '#666',
     textAlign: 'center',
   },
+  input: { borderWidth: 1, borderColor: '#ccc', borderRadius: 8, padding: 10, marginBottom: 12 },
+  passwordContainer: { flexDirection: 'row', alignItems: 'center', borderWidth: 1, borderColor: '#ccc', borderRadius: 8, marginBottom: 12 },
+  passwordInput: { flex: 1, padding: 10 },
+  showHideButton: { paddingHorizontal: 10 },
 });
