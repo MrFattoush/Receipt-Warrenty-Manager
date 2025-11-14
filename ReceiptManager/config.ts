b3c6ef059ee2687581497ca3fdaffd7bdf49a5d1
MrFattoush <fattash76@gmail.com>
2025-11-14 01:08:22 -0600

    OCR implemented!

--- ReceiptManager/config.ts
+++ ReceiptManager/config.ts
@@ -13,7 +13,7 @@ const getApiUrl = () => {
     } else {
       // Mobile device - use your computer's IP
       // Update this IP when you change networks
-      return 'http://10.195.80.45:5001';
+      return 'http://192.168.1.20:5001';
     }
   } else {
     // Production mode - replace with your production server URL
